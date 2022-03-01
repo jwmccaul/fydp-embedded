749c381402b5666ac2486eaede06ea0d452d2639
Brandon Fonseca <brandonfonseca5@gmail.com>
2022-03-01 16:59:45 -0500

    working just low frame rate

--- testing1.py
+++ testing1.py
@@ -2,6 +2,8 @@
 import numpy as np
 import argparse
 import cv2
+from imutils.video import FPS
+from imutils.video import WebcamVideoStream
 
 NUM_ROWS = 5
 NUM_COLS = 11
@@ -18,23 +20,24 @@ def getRowAndCol(maxLoc, radius, rowIntervals, colIntervals, frame_width, frame_
     zero_mask = np.zeros((frame_height,frame_width))
     cv2.circle(zero_mask, maxLoc, radius, (255, 0, 0), -1)
 
-    cv2.imwrite("test.png",zero_mask)
+    # cv2.imwrite("test.png",zero_mask)
 
 
     coords_in_circle = np.transpose(np.nonzero(zero_mask))
+    rows_to_be_on = [0 for i in range(NUM_ROWS)]
+    cols_to_be_on = [0 for i in range(NUM_COLS)]
 
     for coord in coords_in_circle:
-        print(coord)
+        for i in range(len(rowIntervals) - 1):
+            if(coord[0] > rowIntervals[i] and coord[0] <= rowIntervals[i+1]):
+               rows_to_be_on[i] = 1
+        for j in range(len(colIntervals) - 1):
+            if(coord[1] > colIntervals[j] and coord[1] <= colIntervals[j+1]):
+                cols_to_be_on[j] = 1
 
+    return rows_to_be_on, cols_to_be_on
 
-
-
-
-    
-
-
-
-
+            
 
 def detectAndDisplay(frame, frame_width, frame_height, rowIntervals, colIntervals):
     maxLocAboveThresh = -1
@@ -72,22 +75,31 @@ ap = argparse.ArgumentParser()
 ap.add_argument("-r", "--radius", type = int, help = "radius of Gaussian blur; must be odd")
 ap.add_argument("-t", "--threshhold", type = int, help = "Minimum threshhold value to activate (0-255)", default=250)
 ap.add_argument('--camera', help='Camera divide number.', type=int, default=0)
+ap.add_argument("-n", "--num_frames", type=int, default=100,
+	help="# of frames to loop over for FPS test")
+ap.add_argument("-d", "--display", type=int, default=-1,
+	help="Whether or not frames should be displayed")
 args = vars(ap.parse_args())
 
 camera_device = args["camera"]
-#-- 2. Read the video stream
-cap = cv2.VideoCapture(camera_device)
-# cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc("M", "J", "P", "G"))
-# cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
-# cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+
+cap = cv2.VideoCapture('/dev/video0')
+cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc("M", "J", "P", "G"))
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+# cap = WebcamVideoStream(src="/dev/video0").start()
 
 
-if not cap.isOpened:
+
+if not cap.isOpened():
     print('--(!)Error opening video capture')
     exit(0)
 
 frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
 frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+# frame_width = 1280
+# frame_height = 720
 
 rowIntervals, colIntervals = getIntervals(frame_width, frame_height)
 print(rowIntervals, colIntervals)
@@ -97,6 +109,8 @@ print("Height", frame_height)
 
 while True:
     ret, frame = cap.read()
+    # frame = cap.read()
+    
     if frame is None:
         print('--(!) No captured frame -- Break!')
         break
@@ -104,9 +118,15 @@ while True:
     maxLoc, image = detectAndDisplay(frame, frame_width, frame_height, rowIntervals, colIntervals)
 
     if(maxLoc != -1):
-        getRowAndCol(maxLoc, args["radius"], rowIntervals, 
+        rows_to_be_on, cols_to_be_on = getRowAndCol(maxLoc, args["radius"], rowIntervals, 
         colIntervals, frame_width, frame_height, image)
+        print("ROWS TO BE ON", rows_to_be_on)
+        print("COLS TO BE ON", cols_to_be_on)
 
-
-    if cv2.waitKey(10) == 27:
+    # if args["display"] > 0:
+    #     cv2.imshow("Frame", frame)
+    if cv2.waitKey(5) == 27:
         break
+
+
+cv2.destroyAllWindows()
