95dc501db63c242760e35a19f570cbdfc0ef6793
J M <57846092+jwmccaul@users.noreply.github.com>
2022-03-05 12:04:57 -0500

    Create objectDetectionJoe.py

---
+++ objectDetectionJoe.py
@@ -0,0 +1,83 @@
+from __future__ import print_function
+import cv2 as cv
+import argparse
+
+font = cv.FONT_HERSHEY_SIMPLEX
+prev_center = None
+prev_w = 0
+prev_h = 0
+
+def detectAndDisplay(frame):
+    frame_gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    frame_gray = cv.equalizeHist(frame_gray)
+
+    center = None
+    out_w = 600
+    out_h = 600
+
+    #-- Detect faces
+    faces = face_cascade.detectMultiScale(frame_gray)
+    for (x,y,w,h) in faces:
+        center = (x + w//2, y + h//2)
+        frame = cv.ellipse(frame, center, (w//2, h//2), 0, 0, 360, (255, 0, 255), 4)
+
+        out_w = wg
+        out_h = h
+
+        faceROI = frame_gray[y:y+h,x:x+w]
+        #-- In each face, detect eyes
+        eyes = eyes_cascade.detectMultiScale(faceROI)
+        for (x2,y2,w2,h2) in eyes:
+            eye_center = (x + x2 + w2//2, y + y2 + h2//2)
+            radius = int(round((w2 + h2)*0.25))
+            frame = cv.circle(frame, eye_center, radius, (255, 0, 0 ), 4)
+
+    if center == None:
+        center = prev_center
+        frame = cv.ellipse(frame, center, (prev_w//2, prev_h//2), 0, 0, 360, (255, 0, 255), 4)
+        
+    locn = str(center)
+
+    cv.putText(frame,locn,(10,500), font, 2, (255, 255, 255), 2, cv.LINE_AA)
+
+    cv.imshow('Capture - Face detection', frame)
+
+    return center, out_w, out_h
+
+parser = argparse.ArgumentParser(description='Code for Cascade Classifier tutorial.')
+parser.add_argument('--face_cascade', help='Path to face cascade.', default='data/haarcascades/haarcascade_frontalface_alt.xml')
+parser.add_argument('--eyes_cascade', help='Path to eyes cascade.', default='data/haarcascades/haarcascade_eye_tree_eyeglasses.xml')
+parser.add_argument('--camera', help='Camera divide number.', type=int, default=0)
+args = parser.parse_args()
+
+face_cascade_name = args.face_cascade
+eyes_cascade_name = args.eyes_cascade
+
+face_cascade = cv.CascadeClassifier()
+eyes_cascade = cv.CascadeClassifier()
+
+#-- 1. Load the cascades
+if not face_cascade.load(cv.samples.findFile(face_cascade_name)):
+    print('--(!)Error loading face cascade')
+    exit(0)
+if not eyes_cascade.load(cv.samples.findFile(eyes_cascade_name)):
+    print('--(!)Error loading eyes cascade')
+    exit(0)
+
+camera_device = args.camera
+#-- 2. Read the video stream
+cap = cv.VideoCapture(camera_device)
+if not cap.isOpened:
+    print('--(!)Error opening video capture')
+    exit(0)
+
+while True:
+    ret, frame = cap.read()
+    if frame is None:
+        print('--(!) No captured frame -- Break!')
+        break
+
+    prev_center, prev_w, prev_h = detectAndDisplay(frame)
+
+    if cv.waitKey(10) == 27:
+        break
