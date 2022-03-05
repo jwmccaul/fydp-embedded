5ad807d16a4e0e2f02f3f5336e80004ba5766843
brandonfonseca <brandonfonseca5@gmail.com>
2022-03-05 16:02:08 -0500

    get face detection stuff working

--- objectDetectionJoe.py
+++ objectDetectionJoe.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 import cv2 as cv
 import argparse
+import nanocamera as nano
 
 font = cv.FONT_HERSHEY_SIMPLEX
 prev_center = None
@@ -21,7 +22,7 @@ def detectAndDisplay(frame):
         center = (x + w//2, y + h//2)
         frame = cv.ellipse(frame, center, (w//2, h//2), 0, 0, 360, (255, 0, 255), 4)
 
-        out_w = wg
+        out_w = w
         out_h = h
 
         faceROI = frame_gray[y:y+h,x:x+w]
@@ -45,9 +46,9 @@ def detectAndDisplay(frame):
     return center, out_w, out_h
 
 parser = argparse.ArgumentParser(description='Code for Cascade Classifier tutorial.')
-parser.add_argument('--face_cascade', help='Path to face cascade.', default='data/haarcascades/haarcascade_frontalface_alt.xml')
-parser.add_argument('--eyes_cascade', help='Path to eyes cascade.', default='data/haarcascades/haarcascade_eye_tree_eyeglasses.xml')
-parser.add_argument('--camera', help='Camera divide number.', type=int, default=0)
+parser.add_argument('--face_cascade', help='Path to face cascade.', default='haarcascade_frontalface_alt.xml')
+parser.add_argument('--eyes_cascade', help='Path to eyes cascade.', default='haarcascade_eye_tree_eyeglasses.xml')
+parser.add_argument('--camera', help='Camera divide number.', type=str, default="/dev/video2")
 args = parser.parse_args()
 
 face_cascade_name = args.face_cascade
@@ -66,13 +67,13 @@ if not eyes_cascade.load(cv.samples.findFile(eyes_cascade_name)):
 
 camera_device = args.camera
 #-- 2. Read the video stream
-cap = cv.VideoCapture(camera_device)
-if not cap.isOpened:
-    print('--(!)Error opening video capture')
-    exit(0)
+cap = nano.Camera(flip=0)
+# if not cap.isOpened:
+#     print('--(!)Error opening video capture')
+#     exit(0)
 
 while True:
-    ret, frame = cap.read()
+    frame = cap.read()
     if frame is None:
         print('--(!) No captured frame -- Break!')
         break
